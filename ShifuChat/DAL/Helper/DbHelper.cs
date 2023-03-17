86fd56c8786af227eb5e3f174e047c8c4697483d
whereMyCleanCode <anarckkyy@gmail.com>
2023-03-17 15:40:34 +0300

    git ingnore 2 baby

--- ShifuChat/DAL/Helper/DbHelper.cs
+++ ShifuChat/DAL/Helper/DbHelper.cs
@@ -4,6 +4,7 @@ namespace ShifuChat.DAL.Helper
 	public class DbHelper
 	{
 	 public static string conectString = "Host=localhost;Port=5432;Database=test;User ID=postgres;Password=0945;";
+		int w = 2;
     }
 }
 
